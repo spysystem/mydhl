d4bc27c8e391bb85998d16cf831bd8980879f65a
Mads Føhns <mads@spysystem.dk>
2021-08-18 19:59:05 +0200

    Recompile

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 del /s /q docs lib test
 
 docker pull spysystem/openapi-generator:th-spy-master
-docker run --rm -v %cd%:/local spysystem/openapi-generator:th-spy-master generate -i /local/mydhl.yaml -l php -o /local -c /local/config.json
+docker run --rm -v %cd%:/local spysystem/openapi-generator:th-spy-master generate -i /local/mydhl.yaml -g php -o /local -c /local/config.json
 
 git add docs/*
 git add lib/*
