398e86f0fee5c3c614719ab333950b317fe40943
Thomas Hansen <thomas@thomasphansen.dk>
2022-07-24 12:50:49 +0200

    [REFACTOR] Update guzzle to version 7

--- build.cmd
+++ build.cmd
@@ -1,8 +1,15 @@
-del /s /q docs lib test
+del /s /q .openapi-generator docs lib test > nul 2>&1
 
-docker pull spysystem/openapi-generator:th-spy-master
-docker run --rm -v %cd%:/local spysystem/openapi-generator:th-spy-master generate -i /local/mydhl.yaml -g php -o /local -c /local/config.json
+docker pull spysystem/openapi-generator:spy_branch
 
-git add docs/*
-git add lib/*
-git add test/*
+docker run^
+    --rm^
+    -v "%cd%":/local^
+    spysystem/openapi-generator:spy_branch^
+    generate^
+    --generator-name php^
+    --config /local/config.yaml^
+    --input-spec /local/mydhl.yaml^
+    --output /local
+
+git add . > nul 2>&1
